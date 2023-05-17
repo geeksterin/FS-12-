509f11982b0a55cb6bf742a0b57a58444acf2d8b
kartikdoodleblue <kartik.rai@doodleblue.com>
2023-05-17 19:23:58 +0530

    "module1 lectures

---
+++ Solution.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class Solution{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int[] arr = {1,2,3,4,5};
+        int[] arr2 = {2,3,4,5};
+        sum(arr,arr2);
+        // System.out.println(arr);
+        // System.out.println(arr[3]);
+        // System.out.println(arr[]);
+        
+    }
+   public static void sum(int[] arr, int[]arr2){
+    System.out.println(arr);
+    System.out.println(arr2);
+   }
+}
